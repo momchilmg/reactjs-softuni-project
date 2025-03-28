bea80a4b882ac08a4516abf8fb8d3224853cc069
Momchil MG <momchilmg@gmail.com>
2025-03-29 01:25:55 +0200

    load doctors dynamically and check information on the form before create a appointement

--- client/src/components/appointments/Appointement.jsx
+++ client/src/components/appointments/Appointement.jsx
@@ -1,27 +1,51 @@
 import { useParams } from "react-router"
 import Hero from "../bars/HeroBar"
 import { GlobalContext } from "../context/GlobalContext"
-import { useContext, useEffect } from "react"
+import { useContext, useEffect, useState } from "react"
+import Spinner from "../Spinner";
 
 export default function Appointement() {
 
     let params = useParams()
-    
+
     const [openLoginForm, setOpenLoginForm] = useContext(GlobalContext)[0]
     const [authorizedUser, setAuthorizedUser] = useContext(GlobalContext)[1]
     const [openInfoPopup, setOpenInfoPopup] = useContext(GlobalContext)[2]
+    const [isPending, setIsPending] = useState(true)
+    const [members, setMembers] = useState([])
 
     const onLoad = useEffect(() => {
         if (!authorizedUser && !openLoginForm)
             setOpenLoginForm(true)
         document.getElementsByClassName("baseHeroBar")[0].scrollIntoView()
-    },[])    
+
+        fetch('http://localhost:3030/data/members')
+            .then(response => response.json())
+            .then(data => {
+                const result = Object.values(data)
+                setMembers(result)
+                setIsPending(false)
+            })
+            .catch(error => {
+                console.log(error.message)
+                setTimeout(() => { navigation("/") }, 1000)
+            });
+    }, [])
 
     const MakeAppointement = (formData) => {
         let service = formData.get('service')
-        let member = formData.get('service')
+        let member = formData.get('member')
         let date = formData.get('date')
         let time = formData.get('time')
+        if (!isValidDate(date) ||
+            !isValidTime(time) ||
+            !(service > 0) ||
+            !(member > 0) ||
+            !isFutureDateTime(date + ' ' + time)) {
+
+            setOpenInfoPopup("Please, insert a correct information in appointement form!")
+            return
+        }
 
         const options = {
             method: 'POST',
@@ -30,7 +54,6 @@ export default function Appointement() {
                 'Content-Type': 'application/json'
             },
             body: JSON.stringify({
-                "authorId": authorizedUser._id,
                 "authorName": authorizedUser.username,
                 "service": service,
                 "member": member,
@@ -38,8 +61,8 @@ export default function Appointement() {
                 "time": time,
             })
         }
-        
-        fetch(`http://localhost:3030/data/appointements/`, options)
+
+        fetch(`http://localhost:3030/data/appointments/`, options)
             .then(response => response.json())
             .then(data => {
                 if (data.code !== undefined)
@@ -53,68 +76,93 @@ export default function Appointement() {
 
     return (
         <>
+            {isPending && <Spinner />}
             <Hero name="Appointment" url="appointment" />
-            <div className="container-fluid bg-primary bg-appointment mb-5" style={{marginTop: 50 + 'px'}}>
-            <div className="container">
-                <div className="row gx-5">
-                    <div className="col-lg-6 py-5">
-                        <div className="py-5">
-                            <h1 className="display-5 text-white mb-4">We Are A Certified and Award Winning Dental Clinic You Can Trust</h1>
-                            <p className="text-white mb-0">Eirmod sed tempor lorem ut dolores. Aliquyam sit sadipscing kasd ipsum. Dolor ea et dolore et at sea ea at dolor, justo ipsum duo rebum sea invidunt voluptua. Eos vero eos vero ea et dolore eirmod et. Dolores diam duo invidunt lorem. Elitr ut dolores magna sit. Sea dolore sanctus sed et. Takimata takimata sanctus sed.</p>
+            <div className="container-fluid bg-primary bg-appointment mb-5" style={{ marginTop: 50 + 'px' }}>
+                <div className="container">
+                    <div className="row gx-5">
+                        <div className="col-lg-6 py-5">
+                            <div className="py-5">
+                                <h1 className="display-5 text-white mb-4">We Are A Certified and Award Winning Dental Clinic You Can Trust</h1>
+                                <p className="text-white mb-0">Eirmod sed tempor lorem ut dolores. Aliquyam sit sadipscing kasd ipsum. Dolor ea et dolore et at sea ea at dolor, justo ipsum duo rebum sea invidunt voluptua. Eos vero eos vero ea et dolore eirmod et. Dolores diam duo invidunt lorem. Elitr ut dolores magna sit. Sea dolore sanctus sed et. Takimata takimata sanctus sed.</p>
+                            </div>
                         </div>
-                    </div>
-                    <div className="col-lg-6">
-                        <div className="appointment-form h-100 d-flex flex-column justify-content-center text-center p-5">
-                            <h1 className="text-white mb-4">Make Appointment</h1>
-                            <form id="appointmentForm" action={MakeAppointement}>
-                                <div className="row g-3">
-                                    <div className="col-12 col-sm-6">
-                                        <select className="form-select bg-light border-0" style={{height: 55 + 'px'}} id="service" name="service" defaultValue={params.type}>
-                                            <option>Select A Service</option>
-                                            <option value="1">Teeth Whitening</option>
-                                            <option value="2">Dental Implant</option>
-                                            <option value="3">Root Canal</option>
-                                        </select>
-                                    </div>
-                                    <div className="col-12 col-sm-6">
-                                        <select className="form-select bg-light border-0" id="member" name="member" style={{height: 55 + 'px'}}>
-                                            <option defaultValue>Select Doctor</option>
-                                            <option value="1">Doctor 1</option>
-                                            <option value="2">Doctor 2</option>
-                                            <option value="3">Doctor 3</option>
-                                        </select>
-                                    </div>
-                                    <div className="col-12 col-sm-6">
-                                        <div className="date" id="date1" data-target-input="nearest">
-                                            <input type="date"
-                                                className="form-control bg-light border-0 datetimepicker-input"
-                                                placeholder="Appointment Date"
-                                                 id="date"
-                                                 name="date"
-                                                style={{height: 55 + 'px'}} />
+                        <div className="col-lg-6">
+                            <div className="appointment-form h-100 d-flex flex-column justify-content-center text-center p-5">
+                                <h1 className="text-white mb-4">Make Appointment</h1>
+                                <form id="appointmentForm" action={MakeAppointement}>
+                                    <div className="row g-3">
+                                        <div className="col-12 col-sm-6">
+                                            <select className="form-select bg-light border-0" style={{ height: 55 + 'px' }} id="service" name="service" defaultValue={params.type}>
+                                                <option>Select A Service</option>
+                                                <option value="1">Teeth Whitening</option>
+                                                <option value="2">Dental Implant</option>
+                                                <option value="3">Root Canal</option>
+                                            </select>
+                                        </div>
+                                        <div className="col-12 col-sm-6">
+                                            <select className="form-select bg-light border-0" id="member" name="member" style={{ height: 55 + 'px' }}>
+                                                <option defaultValue>Select Doctor</option>
+                                                {members.map(member => <option value={member.id} key={member.id}>{member.name}</option>)}
+                                            </select>
+                                        </div>
+                                        <div className="col-12 col-sm-6">
+                                            <div className="date" id="date1" data-target-input="nearest">
+                                                <input type="date"
+                                                    className="form-control bg-light border-0 datetimepicker-input"
+                                                    placeholder="Appointment Date"
+                                                    id="date"
+                                                    name="date"
+                                                    style={{ height: 55 + 'px' }} />
+                                            </div>
+                                        </div>
+                                        <div className="col-12 col-sm-6">
+                                            <div className="time" id="time1" data-target-input="nearest">
+                                                <input type="time"
+                                                    className="form-control bg-light border-0 datetimepicker-input"
+                                                    placeholder="Appointment Time"
+                                                    id="time"
+                                                    name="time"
+                                                    style={{ height: 55 + 'px' }}
+                                                />
+                                            </div>
+                                        </div>
+                                        <div className="col-12">
+                                            <button className="btn btn-dark w-100 py-3" type="submit">Make Appointment</button>
                                         </div>
                                     </div>
-                                    <div className="col-12 col-sm-6">
-                                        <div className="time" id="time1" data-target-input="nearest">
-                                            <input type="time"
-                                                className="form-control bg-light border-0 datetimepicker-input"
-                                                placeholder="Appointment Time"
-                                                id="time"
-                                                name="time" 
-                                                style={{height: 55 + 'px'}}
-                                            />
-                                        </div>
-                                    </div>
-                                    <div className="col-12">
-                                        <button className="btn btn-dark w-100 py-3" type="submit">Make Appointment</button>
-                                    </div>
-                                </div>
-                            </form>
+                                </form>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
         </>
     )
+}
+
+function isValidDate(dateString) {
+    const d = new Date(dateString);
+    return !isNaN(d.getTime());
+}
+
+function isValidTime(timeString) {
+    const timeRegex = /^([01]\d|2[0-3]):[0-5]\d$/;
+
+    if (!timeRegex.test(timeString)) {
+        return false;
+    }
+
+    return true;
+}
+
+function isFutureDateTime(dateTimeString) {
+    const inputDateTime = new Date(dateTimeString);
+    const currentDateTime = new Date();
+
+    if (isNaN(inputDateTime.getTime())) {
+        return false;
+    }
+
+    return inputDateTime > currentDateTime;
 }
