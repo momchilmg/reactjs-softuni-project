8d0b636ef552349d78380dc0991240e597e89e52
Momchil MG <momchilmg@gmail.com>
2025-03-28 13:21:32 +0200

    fix show menu

--- client/src/components/bars/NavBar.jsx
+++ client/src/components/bars/NavBar.jsx
@@ -1,5 +1,5 @@
 import { Link, useLocation } from 'react-router';
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import { LoginContext } from '../context/LoginContext';
 
 export default function NavBar() {
@@ -7,21 +7,22 @@ export default function NavBar() {
     const location = useLocation()
     const [openLoginForm, setOpenLoginForm] = useContext(LoginContext)[0]
     const [authorizedUser, setAuthorizedUser] = useContext(LoginContext)[1]
-    
+    const [showMenu, setSHowMenu] = useState(false)
+
     const Logout = () => {
         setAuthorizedUser(null)
     }
-    
+
     return (
         <>
             <nav className="navbar navbar-expand-lg bg-white navbar-light shadow-sm px-5 py-3 py-lg-0">
                 <Link to="/" className="navbar-brand p-0">
                     <h1 className="m-0 text-primary"><i className="fa fa-tooth me-2"></i>DentCare</h1>
                 </Link>
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
+                <button className={"navbar-toggler" + (showMenu ? " collapsed" : "")} type="button" onClick={() => { setSHowMenu(!showMenu) }}>
                     <span className="navbar-toggler-icon"></span>
                 </button>
-                <div className="collapse navbar-collapse" id="navbarCollapse">
+                <div className={"collapse navbar-collapse" + (showMenu ? " show" : "")} id="navbarCollapse">
                     <div className="navbar-nav ms-auto py-0">
                         <Link to="/" className={"nav-item nav-link" + (location.pathname === "/" ? " active" : "")}>Home</Link>
                         <Link to="/about" className={"nav-item nav-link" + (location.pathname === "/about" ? " active" : "")}>About</Link>
@@ -30,7 +31,7 @@ export default function NavBar() {
                         <Link to="/contact" className={"nav-item nav-link" + (location.pathname === "/contact" ? " active" : "")}>Contact</Link>
                     </div>
                     <Link to="/appointment" className="btn btn-primary py-2 px-4 ms-3">Appointment</Link>
-                    <Link to="#" className="btn btn-primary py-2 px-4 ms-3" onClick={() => {(authorizedUser ? Logout() : setOpenLoginForm(true))}}>{(authorizedUser ? 'Logout' : 'Login')}</Link>
+                    <Link to="#" className="btn btn-primary py-2 px-4 ms-3" onClick={() => { (authorizedUser ? Logout() : setOpenLoginForm(true)) }}>{(authorizedUser ? 'Logout' : 'Login')}</Link>
                 </div>
             </nav>
         </>
