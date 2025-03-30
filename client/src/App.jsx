f49157999a7606af40b65c94657d7612f0626f91
Momchil MG <momchilmg@gmail.com>
2025-03-30 15:49:23 +0300

    make parameters optional

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -62,9 +62,8 @@ export default function App() {
                 <Routes>
                     <Route index element={<Home />} />
                     <Route path="/contact" element={<Contact />} />
-                    <Route path="/appointment" element={<Appointement />} />
-                    <Route path="/appointment/:type" element={<Appointement />} />
-                    <Route path="/myappointments" element={(authorizedUser ? <PrivateAppointements /> : <Home />)} />
+                    <Route path="/appointment/:type?" element={<Appointement />} />
+                    <Route path="/myappointments/:page?" element={(authorizedUser ? <PrivateAppointements /> : <Home />)} />
                     <Route path="/team" element={<Team />} />
                     <Route path="/team/:id" element={<TeamMemberDetails />} />
                     <Route path="/about" element={<About />} />
