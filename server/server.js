9600d91a987e33db35e06251d24617ed224f7d64
Momchil MG <momchilmg@gmail.com>
2025-03-31 19:44:28 +0300

    add a correct structure to users collection in server.js

--- server/server.js
+++ server/server.js
@@ -1360,16 +1360,22 @@
     		"35c62d76-8152-4626-8712-eeb96381bea8": {
     			email: "peter@abv.bg",
     			username: "Peter",
+    			address: "Sofia, Geo Milev Str. 123",
+    			phone: "0887123456",
     			hashedPassword: "83313014ed3e2391aa1332615d2f053cf5c1bfe05ca1cbcb5582443822df6eb1"
     		},
     		"847ec027-f659-4086-8032-5173e2f9c93a": {
     			email: "george@abv.bg",
     			username: "George",
+                address: "Sofia, Bulgaria Blvd. 45",
+    			phone: "0887654122",
     			hashedPassword: "83313014ed3e2391aa1332615d2f053cf5c1bfe05ca1cbcb5582443822df6eb1"
     		},
     		"60f0cf0b-34b0-4abd-9769-8c42f830dffc": {
     			email: "admin@abv.bg",
     			username: "Admin",
+                address: "Sofia, Alexander Malinov Blvd. 78",
+    			phone: "0889112233",
     			hashedPassword: "fac7060c3e17e6f151f247eacb2cd5ae80b8c36aedb8764e18a41bbdc16aa302"
     		}
     	},
